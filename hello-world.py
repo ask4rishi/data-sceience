6bcd4da88d1f58d63d2684c3c170e4d6f05af305
ask4rishi <47684388+ask4rishi@users.noreply.github.com>
2019-02-16 12:12:36 +0530

    hello-world.py
    hello world in pyton

---
+++ hello-world.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Manansh Singh");
+
+a = 10;
+b = 20;
+
+c = a + b;
+
+print(c, "Sum of a and b, c");
+f=999;
+d=111;
+
+c = f + d;
+
+print(c, "Sum of f and d, c");
+        
